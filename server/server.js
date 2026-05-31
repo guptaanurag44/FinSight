9bca7d3b77a02c0897c6612eb8b82fe83127f210
guptaanurag44 <anuragguptanitw@gmail.com>
2026-05-31 15:12:20 +0530

    Implement authentication routes and middleware for user registration and login

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors"
+import authRoutes from "./routes/auth.js"
 
 const app=express();
 const PORT= process.env.PORT || 5000
@@ -9,6 +10,8 @@ app.use(express.json());
 app.get('/',(req,res)=>{
     res.json({message: "FinSight server running"});
 })
+
+app.use('/api/auth',authRoutes)
 
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`);
